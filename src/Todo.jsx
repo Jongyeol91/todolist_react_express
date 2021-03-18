c77f1faa450d7cab840aea13d8c67ebccd505a1b
jongyeol park <pjr159@gmail.com>
2021-03-19 08:07:33 +0900

    feat: 작성일 추가

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment'
 import { Checkbox, Button, Row, Col, Card } from "antd";
 
 const Todo = ({ todo, toggleCompletion, handleTodoDelete }) => {
@@ -17,6 +18,7 @@ const Todo = ({ todo, toggleCompletion, handleTodoDelete }) => {
         <Card title={todo.id + '. ' + todo.task}>
           <p>{todo.ref && <span>@{todo.ref}</span>}</p>
           완료: <Checkbox checked={todo.completed} onChange={handleToggleCompletion}/>
+          <p>작성일:{moment(todo.createdAt).format('YYYY-MM-DD')}</p>
           <Button onClick={onRemove}>삭제</Button>
         </Card>
         </Col>
